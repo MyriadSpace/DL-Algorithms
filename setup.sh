7547b037f57b44824a7293ab1fc0e43b787304fa
IG LEE <39043516+OnedgeLee@users.noreply.github.com>
2019-06-11 08:36:08 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 # !/usr/bin/env bash
 # Install virtualenv on local pc
-echo passwd | sudo -S -u multi apt-get install virtualenv
+echo passwd | sudo -S -u user apt-get install virtualenv
 
 # Install python3 in a directory named multicamp
 virtualenv -p python3 ~/multicamp
