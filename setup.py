db64b6a07611df19c3668db9369ba8edf68f507c
IG LEE <39043516+OnedgeLee@users.noreply.github.com>
2019-07-09 09:39:02 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 import subprocess
 
-cmd_list = [
-    'sudo apt install virtualenv',
-    'sudo apt install git',
-    'sudo apt install python3-tk',
-    'virtualenv -p python3 ~/multicamp',
-    'source ~/multicamp/bin/activate',
-    'pip3 install --upgrade tensorflow matplotlib ipykernel jupyter music21 gym',
+install_cmd_list = [
+    'sudo apt -y install virtualenv',
+    'sudo apt -y install git',
+    'sudo apt -y install python3-tk',
+    'virtualenv -p python3 /home/user/multicamp',
+    'source /home/user/multicamp/bin/activate',
+    'pip install --upgrade tensorflow matplotlib ipykernel jupyter music21 gym',
     'python3 -m ipykernel install --user',
     'git clone https://github.com/aidentify/lecture',
 ]
 
-for cmd in cmd_list:
-    install_proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+for install_cmd in install_cmd_list:
+    install_proc = subprocess.Popen(install_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
     while install_proc.poll() is None:
         out = install_proc.stdout.readline()
         print(out.decode('utf-8'), end='')
