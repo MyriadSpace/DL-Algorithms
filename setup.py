fa910f30905165017eb892998d41cc35484a5bc7
IG LEE <39043516+OnedgeLee@users.noreply.github.com>
2019-07-09 10:29:35 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,9 @@
 import subprocess
 
-dpkg_cmd_list = [
+install_cmd_list = [
     'echo "passwd" | sudo -S rm /var/lib/dpkg/lock',
     'echo "passwd" | sudo -S dpkg --configure -a',
-    'echo "passwd" | sudo -S apt update'
-]
-for dpkg_cmd in dpkg_cmd_list:
-    dpkg_proc = subprocess.Popen(dpkg_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
-    while dpkg_proc.poll() is None:
-        out = dpkg_proc.stdout.readline()
-        print(out.decode('utf-8'), end='')
-
-install_cmd_list = [
+    'echo "passwd" | sudo -S apt update',
     'echo "passwd" | sudo -S apt -y install virtualenv git python3-tk',
     'virtualenv -p python3 /home/user/multicamp',
     '/home/user/multicamp/bin/pip3 install --upgrade tensorflow matplotlib ipykernel jupyter music21 gym',
