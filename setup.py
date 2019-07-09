3bb78c4e2c599f329e6f0d4255d05ea4f6c69f8f
IG LEE <39043516+OnedgeLee@users.noreply.github.com>
2019-07-09 10:05:01 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,35 +1,26 @@
 import subprocess
 
-
-dellock_proc = subprocess.Popen('echo "passwd" | sudo -S rm /var/lib/dpkg/lock', stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
-while dellock_proc.poll() is None:
-    out = dellock_proc.stdout.readline()
+dpkg_cmd_list = [
+    'echo "passwd" | sudo -S rm /var/lib/dpkg/lock;',
+    'echo "passwd" | sudo -S dpkg --configure -a;',
+    'echo "passwd" | sudo -S apt update;'
+]
+dpkg_cmd = ''.join(dpkg_cmd_list)
+dpkg_proc = subprocess.Popen(dpkg_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+while dpkg_proc.poll() is None:
+    out = dpkg_proc.stdout.readline()
     print(out.decode('utf-8'), end='')
-
-reconf_proc = subprocess.Popen('echo "passwd" | sudo -S dpkg --configure -a', stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
-while reconf_proc.poll() is None:
-    out = reconf_proc.stdout.readline()
-    print(out.decode('utf-8'), end='')
-
-update_proc = subprocess.Popen('echo "passwd" | sudo -S apt update', stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
-while update_proc.poll() is None:
-    out = update_proc.stdout.readline()
-    print(out.decode('utf-8'), end='')
-
 
 install_cmd_list = [
-    'echo "passwd" | sudo -S apt -y install virtualenv',
-    'echo "passwd" | sudo -S apt -y install git',
-    'echo "passwd" | sudo -S apt -y install python3-tk',
-    'virtualenv -p python3 /home/user/multicamp',
-    '/home/user/multicamp/bin/activate',
-    'pip install --upgrade tensorflow matplotlib ipykernel jupyter music21 gym',
-    'python3 -m ipykernel install --user',
-    'git clone https://github.com/aidentify/lecture',
+    'echo "passwd" | sudo -S apt -y install virtualenv git python3-tk;',
+    'virtualenv -p python3 /home/user/multicamp;',
+    '/home/user/multicamp/bin/activate;',
+    'pip install --upgrade tensorflow matplotlib ipykernel jupyter music21 gym;',
+    'python3 -m ipykernel install --user;',
+    'git clone https://github.com/aidentify/lecture;',
 ]
-
-for install_cmd in install_cmd_list:
-    install_proc = subprocess.Popen(install_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
-    while install_proc.poll() is None:
-        out = install_proc.stdout.readline()
-        print(out.decode('utf-8'), end='')
+install_cmd = ''.join(install_cmd_list)
+install_proc = subprocess.Popen(install_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+while install_proc.poll() is None:
+    out = install_proc.stdout.readline()
+    print(out.decode('utf-8'), end='')
