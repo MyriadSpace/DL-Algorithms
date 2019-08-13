9c933bf8b8e5c6a862c6af9e790e1e057adb6c2b
IG LEE <39043516+OnedgeLee@users.noreply.github.com>
2019-08-13 11:23:04 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ install_cmd_list = [
     'virtualenv -p python3 /home/user/multicamp',
     '/home/user/multicamp/bin/pip3 install --upgrade tensorflow matplotlib ipykernel jupyter music21 gym',
     '/home/user/multicamp/bin/python3 -m ipykernel install --user',
-    'git clone https://github.com/aidentify/lecture',
 ]
 for install_cmd in install_cmd_list:
     install_proc = subprocess.Popen(install_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
