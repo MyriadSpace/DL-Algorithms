ba6d81eb4698540f75aba7a5962b549ffe44166a
IG LEE <39043516+OnedgeLee@users.noreply.github.com>
2019-10-11 14:20:46 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import subprocess
 install_cmd_list = [
     'echo "passwd" | sudo -S apt -y install virtualenv git python3-tk',
     'virtualenv -p python3 /home/user/multicamp',
-    '/home/user/multicamp/bin/pip3 install --upgrade tensorflow matplotlib ipykernel jupyter music21 gym',
+    '/home/user/multicamp/bin/pip3 install --upgrade tensorflow==1.14 matplotlib ipykernel jupyter music21 gym',
     '/home/user/multicamp/bin/python3 -m ipykernel install --user',
 ]
 for install_cmd in install_cmd_list:
